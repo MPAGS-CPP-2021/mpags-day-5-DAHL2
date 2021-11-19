2e3b852f1966d5ab7da4a8613ad8aee81a2005a6
Dennis Lindebaum <dennis.lindebaum@bristol.ac.uk>
2021-11-19 10:56:04 +0000

    Extra comments for clarity

--- src/MPAGSCipher/VigenereCipher.cpp
+++ src/MPAGSCipher/VigenereCipher.cpp
@@ -21,8 +21,6 @@ void VigenereCipher::setKey(const std::string& key)
 
     using lookupEntry = std::pair<char, CaesarCipher>;
 
-    CaesarCipher char_cipher{0};
-
     for (char const& c : key_) {
         // Don't need to add duplicates of entries to the map
         if (charLookup_.count(c) > 0) {
@@ -32,10 +30,8 @@ void VigenereCipher::setKey(const std::string& key)
         // Find the letter position in the alphabet
         char_key = Alphabet::alphabet.find(c);
 
-        char_cipher = CaesarCipher(char_key);
-
         // Add the character and corresponding cipher to the map
-        charLookup_.insert(lookupEntry{c, char_cipher});
+        charLookup_.insert(lookupEntry{c, CaesarCipher(char_key)});
     }
 }
 
@@ -74,10 +70,14 @@ std::string VigenereCipher::applyCipher(const std::string& inputText,
 
     // For each letter in input:
     for (size_t i{0}; i < inputText.size(); i++) {
+        // Need input character as a string for CaesarCipher
         input_char_string = inputText[i];
 
+        // Modulus to make the key repeat as needed
         key_char = key_[i % key_size_];
 
+        // We lookup the cipher at the given character, then apply that cipher
+        //  with the input letter as a string
         outputText +=
             charLookup_.at(key_char).applyCipher(input_char_string, cipherMode);
     }
